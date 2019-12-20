334f2bb1fc0089bb7bebedd88bcb676f9ac5c070
James Polley <jp@jamezpolley.com>
2019-12-20 17:00:30 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -3,31 +3,37 @@ require 'mechanize'
 
 agent = Mechanize.new
 
-url = "https://www.banyule.vic.gov.au/Services/Planning/Planning-Applications-on-Public-Notice-Advertising/Planning-Applications-on-Public-Notice-Register/"
+baseurl = "https://www.banyule.vic.gov.au/Planning-building/Review-local-planning-applications/Advertised-planning-applications"
+pageindex=1
 comment_url = "mailto:enquiries@banyule.vic.gov.au"
 
-page = agent.get(url)
-
-nopage = true
-
-page.search('.listing-results+.list-container .list-item-container a').each_with_index do |application, index|
-  detail_page = agent.get(application.attributes['href'].to_s)
-  notice_date = application.search('p').inner_text.strip.split(/Final da(y|te) of notice: /)[2]
-  header = detail_page.search('h1.oc-page-title').inner_text.strip.to_s
-  council_reference = header.split(/(.*) - (.*)/)[2].to_s
-  address = detail_page.search('p:contains("View Map")').inner_text.split("View Map")[0].gsub("\u00A0", " ").strip.to_s + " VIC"
+url = baseurl + "?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex=#{pageindex})"
+loop do
+  page = agent.get(url)
+  break unless page
   
-  record = {
-    "council_reference" => council_reference,
-    "address" => address,
-    "description" => detail_page.search('.project-details-list+p').inner_text.strip.to_s,
-    "info_url"    => application.attributes['href'].to_s,
-    "comment_url" => comment_url,
-    "date_scraped" => Date.today.to_s,
-    "on_notice_to" => DateTime.parse(notice_date).to_date.to_s
-  }
+  page.search('.listing-results+.list-container .list-item-container a').each_with_index do |application, index|
+    detail_page = agent.get(application.attributes['href'].to_s)
+    notice_date = application.search('p').inner_text.strip.split(/Final da(y|te) of notice: /)[2]
+    header = detail_page.search('h1.oc-page-title').inner_text.strip.to_s
+    council_reference = header.split(/(.*) - (.*)/)[2].to_s
+    address = detail_page.search('p:contains("View Map")').inner_text.split("View Map")[0].gsub("\u00A0", " ").strip.to_s + " VIC"
+  
+    record = {
+      "council_reference" => council_reference,
+      "address" => address,
+      "description" => detail_page.search('.project-details-list+p').inner_text.strip.to_s,
+      "info_url"    => application.attributes['href'].to_s,
+      "comment_url" => comment_url,
+      "date_scraped" => Date.today.to_s,
+      "on_notice_to" => DateTime.parse(notice_date).to_date.to_s
+    }
 
-  puts "Saving record " + record['council_reference'] + " - " + record['address']
-    #puts record
-  ScraperWiki.save_sqlite(['council_reference'], record)
+    puts "Saving record " + record['council_reference'] + " - " + record['address']
+      #puts record
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  end
+  
+  pageindex = pageindex + 1
+  url = baseurl + "?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex=#{pageindex})"
 end
