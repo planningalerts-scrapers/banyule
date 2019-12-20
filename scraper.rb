f1d5bb846623946e5bd5211825395dbc6116b248
James Polley <jp@jamezpolley.com>
2019-12-20 17:24:35 +1100

    check for nillity of council_reference

--- scraper.rb
+++ scraper.rb
@@ -9,19 +9,22 @@ comment_url = "mailto:enquiries@banyule.vic.gov.au"
 
 page = agent.get(url)
 
-loop do
+loop do 
   page.search('.listing-results+.list-container .list-item-container a').each_with_index do |application, index|
     detail_page = agent.get(application.attributes['href'].to_s)
     notice_date = application.search('p').inner_text.strip.split(/Final da(y|te) of notice: /)[2]
     header = detail_page.search('h1.oc-page-title').inner_text.strip.to_s
     council_reference = header.split(/(.*) - (.*)/)[2].to_s
+    puts council_reference
     unless council_reference
       puts "Fallback council_reference finding: #{header}"
       council_reference = header.split(/(.* )(P[0-9]+\/[0-9]{4})/)[1].to_s
       puts "Found #{council_reference}"
     end
+
     address = detail_page.search('p:contains("View Map")').inner_text.split("View Map")[0].gsub("\u00A0", " ").strip.to_s + " VIC"
-  
+    puts address
+    
     record = {
       "council_reference" => council_reference,
       "address" => address,
@@ -33,7 +36,7 @@ loop do
     }
 
     puts "Saving record " + record['council_reference'] + " - " + record['address']
-      #puts record
+    puts record
     ScraperWiki.save_sqlite(['council_reference'], record)
   end
   
