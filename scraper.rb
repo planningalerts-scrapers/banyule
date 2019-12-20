f93d1adcfcc048513ef613c71c3d0f5acf29e306
James Polley <jp@jamezpolley.com>
2019-12-20 16:37:58 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -13,8 +13,7 @@ nopage = true
 page.search('.listing-results+.list-container .list-item-container a').each_with_index do |application, index|
   detail_page = agent.get(application.attributes['href'].to_s)
   notice_date = application.search('p').inner_text.strip.split(/Final da(y|te) of notice: /)[2]
-  puts detail_page.search('h3')
-  header = detail_page.search('h3 .oc-page-title').inner_text.strip.to_s
+  header = detail_page.search('h1 .oc-page-title').inner_text.strip.to_s
   council_reference = header.split(/(.*) - (.*)/)[1].to_s
   puts detail_page.search('o:contains("View Map")')
   address = detail_page.search('o:contains("View Map")').inner_text.gsub("\u00A0", " ").strip.to_s + " VIC",
@@ -22,7 +21,7 @@ page.search('.listing-results+.list-container .list-item-container a').each_with
   record = {
     "council_reference" => council_reference,
     "address" => address + " VIC",
-    "description" => detail_page.search('h3:contains("Description:") span').inner_text.strip.to_s,
+    "description" => detail_page.search('.main-content p:first-of-type').inner_text.strip.to_s,
     "info_url"    => application.attributes['href'].to_s,
     "comment_url" => comment_url,
     "date_scraped" => Date.today.to_s,
